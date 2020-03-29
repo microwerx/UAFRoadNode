4e7761c903906ef5675f3e7b8811fa2e054b1bae
Nami Kim <namikim0917@gmail.com>
2020-03-29 10:16:40 -0800

    Add location search feature

--- UAFRoadNode/ViewController.swift
+++ UAFRoadNode/ViewController.swift
@@ -4,45 +4,47 @@
 //
 //  Created by Alex Lewandowski  on 2/10/20.
 //  Copyright © 2020 UAFRoadNode. All rights reserved.
-//
+
+
 
 import UIKit
-import GoogleMaps
-import GooglePlaces
+import CoreLocation
 
-class ViewController: UIViewController, GMSMapViewDelegate {
+class ViewController: UIViewController {
+
     
+    let locationManager = CLLocationManager()
+    
+    @IBOutlet weak var lblLocation: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        getCurrentLocation()
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-    }
-        
-//    //var mapView: GMSMapView?
-//
-//    override func loadView() {
-//        //super.viewDidLoad()
-//
-////        // Create a GMSCameraPosition that tells the map to display the
-////        // coordinate 64.855793, -147.833746
-////        let camera = GMSCameraPosition.camera(withLatitude: 64.855793, longitude: -147.833746, zoom: 10.0)
-////        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
-////        mapView.delegate = self
-////        self.view = mapView
-////        //view = mapView
-////
-////        // Creates a marker in the center of the map.
-////        let marker = GMSMarker()
-////        marker.position = CLLocationCoordinate2D(latitude: 64.855793, longitude:-147.833746)
-////        marker.title = "UAF"
-////        marker.snippet = "MarkerExample"
-////        marker.map = mapView
-////        marker.isDraggable = false //users cannot move the node
-//  }
+    func getCurrentLocation() {
+        // Ask for Authorisation from the User.
+        self.locationManager.requestAlwaysAuthorization()
 
+        // For use in foreground
+        self.locationManager.requestWhenInUseAuthorization()
+
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+        }
+    }
+
+}
+
+extension ViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+           guard let locValue: CLLocationCoordinate2D = manager.location?.coordinate else { return }
+           print("locations = \(locValue.latitude) \(locValue.longitude)")
+        lblLocation.text = "latitude = \(locValue.latitude), longitude = \(locValue.longitude)"
+    }
 }
 
 
--- UAFRoadNode/showMap.swift
+++ UAFRoadNode/showMap.swift
@@ -4,35 +4,76 @@
 //
 //  Created by Nami Kim on 2/24/20.
 //  Copyright © 2020 UAFRoadNode. All rights reserved.
-//
 
 import UIKit
 import GoogleMaps
 import GooglePlaces
 
-class showMap: UIViewController, GMSMapViewDelegate {
-    var mapView: GMSMapView?
+class showMap: UIViewController {
 
-    @IBOutlet weak var showMap: UIView!
-    override func loadView() {
+    @IBOutlet weak var mapView: GMSMapView!
+    @IBOutlet weak var txtSearch: UITextField!
+    
+    @IBAction func locationTapped(_ sender: Any) {
+        gotoPlaces()
+    }
+    override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Create a GMSCameraPosition that tells the map to display the
-        // coordinate 64.855793, -147.833746
-        let camera = GMSCameraPosition.camera(withLatitude: 64.855793, longitude: -147.833746, zoom: 10.0)
-        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
-        mapView.delegate = self
-        self.view = mapView
-        //view = mapView
-        self.mapView?.addSubview(mapView)
-
-        // Creates a marker in the center of the map.
-        let marker = GMSMarker()
-        marker.position = CLLocationCoordinate2D(latitude: 64.855793, longitude:-147.833746)
-        marker.title = "UAF"
-        marker.snippet = "MarkerExample"
-        marker.map = mapView
-        marker.isDraggable = false //users cannot move the node
-  }
-
+        // Do any additional setup after loading the view.
+        // GOOGLE MAPS SDK: COMPASS
+        mapView.settings.compassButton = true
+        
+        // GOOGLE MAPS SDK: USER'S LOCATION
+         mapView.isMyLocationEnabled = true
+         mapView.settings.myLocationButton = true
+    }
+    
+    func gotoPlaces() {
+        txtSearch.resignFirstResponder()
+        let acController = GMSAutocompleteViewController()
+        acController.delegate = self
+        present(acController, animated: true, completion: nil)
+    }
 }
+
+extension showMap: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+           guard let locValue: CLLocationCoordinate2D = manager.location?.coordinate else { return }
+           print("locations = \(locValue.latitude) \(locValue.longitude)")
+    }
+}
+
+extension showMap: GMSAutocompleteViewControllerDelegate {
+    func viewController(_ viewController: GMSAutocompleteViewController, didAutocompleteWith place: GMSPlace) {
+         print("Place name: \(String(describing: place.name))")
+       dismiss(animated: true, completion: nil)
+       
+       self.mapView.clear()
+       self.txtSearch.text = place.name
+    
+   
+       let cord2D = CLLocationCoordinate2D(latitude: (place.coordinate.latitude), longitude: (place.coordinate.longitude))
+       
+       let marker = GMSMarker()
+       marker.position =  cord2D
+       marker.title = place.name
+       marker.snippet = "node"
+       
+       let markerImage = UIImage(named: "node_pin")!
+       let markerView = UIImageView(image: markerImage)
+       marker.iconView = markerView
+       marker.map = self.mapView
+       
+       self.mapView.camera = GMSCameraPosition.camera(withTarget: cord2D, zoom: 15)
+    }
+    
+    func viewController(_ viewController: GMSAutocompleteViewController, didFailAutocompleteWithError error: Error) {
+        print(error.localizedDescription)
+    }
+    
+    func wasCancelled(_ viewController: GMSAutocompleteViewController) {
+        
+    }
+}
+
