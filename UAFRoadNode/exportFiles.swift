7f0bcd4b202f20ee4f0171ff55233d7c680bc953
Nami Kim <namikim0917@gmail.com>
2020-04-19 22:03:25 -0800

    Modify buttons and constraints for all views

---
+++ UAFRoadNode/exportFiles.swift
@@ -0,0 +1,31 @@
+//
+//  exportFiles.swift
+//  UAFRoadNode
+//
+//  Created by Nami Kim on 4/19/20.
+//  Copyright © 2020 UAFRoadNode. All rights reserved.
+//
+// Will not be implemented but a dummy function will be written 
+
+import UIKit
+
+class exportFiles: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
