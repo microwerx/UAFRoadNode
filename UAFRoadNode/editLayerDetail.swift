46f20e038eada72acbb6de27d1758d1fde29dfab
Nami Kim <namikim0917@gmail.com>
2020-04-19 22:28:17 -0800

    Fix editLayerSegue problem

--- UAFRoadNode/editLayerDetail.swift
+++ UAFRoadNode/editLayerDetail.swift
@@ -10,8 +10,8 @@ import UIKit
 
 class editLayerDetail: UIViewController {
 
+  
     @IBOutlet weak var layerName: UILabel!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
