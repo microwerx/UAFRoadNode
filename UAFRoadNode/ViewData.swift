5a26da5de94fa66a2a43216f5b2d300af0b1d0a2
Nami Kim <namikim0917@gmail.com>
2020-04-27 12:41:38 -0800

    Initial form for view node data

--- UAFRoadNode/ViewData.swift
+++ UAFRoadNode/ViewData.swift
@@ -9,22 +9,36 @@
 import UIKit
 
 class ViewData: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
     }
-    */
+}
 
+extension ViewData: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath as IndexPath) as! viewDataTableViewCell
+        cell.attributeType.text = "1" // fill in your value for column 1 (e.g. from an array)
+        cell.attributeValue.text = "2" // fill in your value for column 2
+        
+        return cell
+    }
+    
 }
---
+++ UAFRoadNode/viewDataTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  viewDataTableViewCell.swift
+//  UAFRoadNode
+//
+//  Created by Nami Kim on 4/27/20.
+//  Copyright © 2020 UAFRoadNode. All rights reserved.
+//
+
+import UIKit
+
+class viewDataTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var attributeType: UILabel!
+    @IBOutlet weak var attributeValue: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
