95a42f895f56be6df48f3bc43593910977007c37
Nami Kim <namikim0917@gmail.com>
2020-04-27 11:23:20 -0800

    Initial commit for addData tableview

--- UAFRoadNode/AddData.swift
+++ UAFRoadNode/AddData.swift
@@ -8,23 +8,52 @@
 
 import UIKit
 
-class AddData: UIViewController {
-
+class AddData: UIViewController, UITextFieldDelegate, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    let animals: [String] = ["Test1", "Test2", "Test3", "Test4", "Test5"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view, typically from a nib.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    var allCellsText = [String?](repeating: nil, count:5)
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return animals.count
     }
-    */
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath as IndexPath) as! addDataTableViewCell
+        cell.addDataTextField?.delegate = self
+        cell.addDataTextField?.text = ""
+        cell.addDataTextField?.placeholder = animals[indexPath.row]
+        cell.addDataTextField?.autocorrectionType = UITextAutocorrectionType.no
+        cell.addDataTextField?.autocapitalizationType = UITextAutocapitalizationType.none
+        cell.addDataTextField?.adjustsFontSizeToFitWidth = true;
+        return cell
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        let indexOf = animals.firstIndex(of:textField.placeholder!)
+        print(indexOf as Any)
+        if(textField.placeholder! == animals[indexOf!]){
+            if( indexOf! <= (allCellsText.count - 1)){
+                allCellsText.remove(at: indexOf!)
+            }
+            allCellsText.insert(textField.text!, at: indexOf!)
+            print(allCellsText)
+        }
+    }
+    
+    //delegate method
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {           textField.resignFirstResponder()
+        return true
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
 }
+
---
+++ UAFRoadNode/addDataTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  addDataTableViewCell.swift
+//  UAFRoadNode
+//
+//  Created by Nami Kim on 4/27/20.
+//  Copyright © 2020 UAFRoadNode. All rights reserved.
+//
+
+import UIKit
+
+class addDataTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var attributeData: UILabel!
+    @IBOutlet weak var addDataTextField: UITextField!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
