a198897f95aec1adf2f0ef43f0b14f7f8ef529d7
Alex Lewandowski <aflewandowski@alaska.edu>
2020-04-29 00:06:27 -0800

    Add layer and node info to select datapoint to view screen

--- UAFRoadNode/SelectDataPoint.swift
+++ UAFRoadNode/SelectDataPoint.swift
@@ -13,6 +13,8 @@ var selectedDateTime = ""
 
 class SelectDataPoint: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
+    @IBOutlet weak var node_label: UILabel!
+    @IBOutlet weak var layer_label: UILabel!
     @IBOutlet weak var myTableView: UITableView!
     
     let dataPoints = DBManager.shared.getDataPoints(node_id: selected_node)
@@ -37,6 +39,8 @@ class SelectDataPoint: UIViewController, UITableViewDelegate, UITableViewDataSou
         build_date_labels()
         myTableView.dataSource = self
         myTableView.delegate = self
+        node_label.text = DBManager.shared.getNodeName(node_id: selected_node)
+        layer_label.text = DBManager.shared.getLayerName(node_id: selected_node)
 
         // Do any additional setup after loading the view.
     }
